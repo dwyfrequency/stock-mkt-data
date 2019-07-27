9839e33137d2a364ae5c94a7f711bfddea708d61
dwyfrequency <jdwy215@gmail.com>
2019-07-27 10:49:29 -0400

    adding initial display elements and search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Dashboard from './components/reuse/Dashboard';
 
 const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
 };
 
 export default App;
